cc3af35201c7e0078113e2257a54b917e937851c
nick.wessing <nick.wessing@centare.com>
2015-08-29 09:14:08 -0500

    Fix Comment POST, rename .js to .jsx

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
+var server = require('./server');
 
-var jsxSrc = './public/js/*.js';
+var jsxSrc = './public/js/*.jsx';
 gulp.task('jsx', function () {
 	return gulp.src(jsxSrc)
 		.pipe(react())
 		.pipe(gulp.dest('./public/build/'));
 });
 
-gulp.task('default', function () {
+gulp.task('serve', function () {
+	server.start();	
+});
+
+gulp.task('default', ['serve'], function () {
 	gulp.watch(jsxSrc, ['jsx']);
 });
--- server.js
+++ server.js
@@ -3,52 +3,55 @@ var http = require('http'),
     fs = require('fs')
     ;
 
-http.createServer(function (request, response) {
-    var pathname = url.parse(request.url).pathname;
-    // console.log(pathname);
-    if (pathname === '/') {
-        fs.readFile('./public/index.html', function (error, data) {
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data);
-            response.end();
-        });
-    } else if (pathname === '/comments.json') {
-        if (request.method === 'POST') {
-            debugger;
-            var postData = '';
-            request.on('data', function (chunk) {
-                postData += chunk;
-            });
-            request.on('end', function () {
-                fs.readFile('./comments.json', function (error, data) {
-                    var comments = JSON.parse(data);
-                    comments.push(JSON.parse(postData));
-                    var commentsData = JSON.stringify(comments);
-                    fs.writeFile('./comments.json', JSON.stringify(commentsData), function () {
-                        response.writeHead(200, {'Content-Type': 'application/json'});
-                        response.write(commentsData);
-                        response.end();
-                    });
-                });
-            });
-        } else {
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            fs.readFile('./comments.json', function (error, data) {
+function start() {
+    http.createServer(function (request, response) {
+        var pathname = url.parse(request.url).pathname;
+        // console.log(pathname);
+        if (pathname === '/') {
+            fs.readFile('./public/index.html', function (error, data) {
+                response.writeHead(200, {'Content-Type': 'text/html'});
                 response.write(data);
                 response.end();
             });
+        } else if (pathname === '/comments.json') {
+            if (request.method === 'POST') {
+                var postData = '';
+                request.on('data', function (chunk) {
+                    postData += chunk;
+                });
+                request.on('end', function () {
+                    fs.readFile('./comments.json', function (error, data) {
+                        var comments = JSON.parse(data.toString());
+                        comments.push(JSON.parse(postData));
+                        var commentsData = JSON.stringify(comments, null, 4);
+                        fs.writeFile('./comments.json', commentsData, function () {
+                            response.writeHead(200, {'Content-Type': 'application/json'});
+                            response.write(commentsData);
+                            response.end();
+                        });
+                    });
+                });
+            } else {
+                response.writeHead(200, {'Content-Type': 'application/json'});
+                fs.readFile('./comments.json', function (error, data) {
+                    response.write(data);
+                    response.end();
+                });
+            }
+    
+        } else if (pathname.substr(0, 6) === '/build') {
+            response.writeHead(200, {'Content-Type': 'application/javascript'});
+            fs.readFile('./public' + pathname, function (error, data) {
+                response.write(data);
+                response.end();
+            });
+        } else {
+            response.writeHead(404);
+            response.write('FILE NOT FOUND');
+            response.end();
         }
 
-    } else if (pathname.substr(0, 6) === '/build') {
-        response.writeHead(200, {'Content-Type': 'application/javascript'});
-        fs.readFile('./public' + pathname, function (error, data) {
-            response.write(data);
-            response.end();
-        });
-    } else {
-        response.writeHead(404);
-        response.write('FILE NOT FOUND');
-        response.end();
-    }
+    }).listen(8888);
+}
 
-}).listen(8888);
+exports.start = start;
